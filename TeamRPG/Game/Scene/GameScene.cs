8736895281a966f962c387fa778044bea87c78fb
khh7052 <59150989+khh7052@users.noreply.github.com>
2025-07-18 20:40:03 +0900

    [Feat] 랜덤 인카운터 전투씬 연결되도록 변경

--- TeamRPG/Game/Scene/GameScene.cs
+++ TeamRPG/Game/Scene/GameScene.cs
@@ -86,7 +86,7 @@ namespace TeamRPG.Game.Scene
             var initialEnemies = EnemyManager.GetInstance().GetInitialEnemies();
 
             for(int i = 0; i < initialEnemies.Count; i++)
-                EnemyManager.GetInstance().AddEnemy(initialEnemies[0].Item1, initialEnemies[0].Item2);
+                EnemyManager.GetInstance().AddEnemy(initialEnemies[i].Item1, initialEnemies[i].Item2);
         }
 
         public void Update()
